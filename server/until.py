02f626e9ed9f9eda2ea8134a702639ff0c7b3d1f
00sukhjeet00 <sukhjeet20002@gmail.com>
2021-09-17 17:34:01 +0530

    Data Science

---
+++ server/until.py
@@ -0,0 +1,33 @@
+import pickle
+import json
+import numpy as np
+__location=None
+__data_columns=None
+__model=None
+def get_price(location,sqft,bhk,bath):
+    global __data_columns
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0],2)
+def get_locations():
+    global __location
+    return __location
+def load_saved_location():
+    global __location
+    global __data_columns
+    global __model
+    with open('./columns.json','r') as f:
+        __data_columns=json.load(f)['data_columns']
+        __location=__data_columns[3:]
+    with open('./bengalore_pickle','rb') as f:
+        __model=pickle.load(f)
+if __name__ == '__main__':
+    load_saved_location()
